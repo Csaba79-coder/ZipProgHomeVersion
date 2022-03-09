69f5746206bf8e276eda78ad9f996cfb92e28ebd
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-09 15:15:23 +0100

    create test for ZipController

---
+++ src/test/java/controller/ZipControllerTest.java
@@ -0,0 +1,26 @@
+package controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZipControllerTest {
+
+    String testPath1TxtMoreFiles = "C:\\Users\\Computer\\Documents\\Java\\Progmatic\\ZipProgHomeVersionGradle\\src\\main\\resources\\source_txt_dir\\source_duplicated_txt_dir";
+    String testFileName = "test1.txt";
+    String testZipFilePath1 = "C:\\Users\\Computer\\Documents\\Java\\Progmatic\\ZipProgHomeVersionGradle\\src\\main\\resources\\source_zip_dir\\VadaszCsaba_01.zip";
+    String testResultFolderUnsuccessful = "C:\\Users\\Computer\\Desktop\\proba";
+
+    ZipController zipController;
+
+    @BeforeEach
+    void setUp() {
+        zipController = new ZipController();
+    }
+
+    @Test
+    void unzipFile() {
+        assertFalse(zipController.unzipFile(testPath1TxtMoreFiles, testFileName, testZipFilePath1, testResultFolderUnsuccessful));
+    }
+}
