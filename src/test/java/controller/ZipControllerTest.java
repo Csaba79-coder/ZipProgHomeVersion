dd1a4eb57def7fde8e66b2383299e0543a751d97
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-03-09 15:29:08 +0100

    refactor resultFolder in test

--- src/test/java/controller/ZipControllerTest.java
+++ src/test/java/controller/ZipControllerTest.java
@@ -10,7 +10,7 @@ class ZipControllerTest {
     String testPath1TxtMoreFiles = "C:\\Users\\Computer\\Documents\\Java\\Progmatic\\ZipProgHomeVersionGradle\\src\\main\\resources\\source_txt_dir\\source_duplicated_txt_dir";
     String testFileName = "test1.txt";
     String testZipFilePath1 = "C:\\Users\\Computer\\Documents\\Java\\Progmatic\\ZipProgHomeVersionGradle\\src\\main\\resources\\source_zip_dir\\VadaszCsaba_01.zip";
-    String testResultFolderUnsuccessful = "C:\\Users\\Computer\\Desktop\\proba";
+    String testResultFolderUnsuccessful = "C:\\Users\\Computer\\Desktop\\unsuccessful";
 
     ZipController zipController;
 
